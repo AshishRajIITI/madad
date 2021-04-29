57267ada0c7d8b1e28900b3c3cecc2f5f9916dbb
AshishRajIITI <ashishiiti19@gmail.com>
2021-04-29 16:02:43 +0530

    added folder structure for frontend

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+
+
+const Main = () =>{
+
+        return(
+            <div>
+                    <Header />
+                    <Switch>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                    <Footer />
+           </div>
+        );
+}
+
+export default Main;
---
+++ frontend/src/components/SeekerLandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DonorLandingPage = () =>{
+
+        return(
+            <h1>donor landing page</h1>
+        );
+}
+
+export default DonorLandingPage;
