300b6f873f3c895bcaa84424b5bb56af8f20d725
pallavi1812 <upallavi1812@gmail.com>
2021-04-29 21:16:56 +0530

    added footer

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
-import Footer from "./Footer";
+import Footer from "./Footer/Footer";
 import Home from "./Home";
 
 
-const Main = () =>{
+const Main = () => {
 
-        return(
-            <div>
-                    <Header />
-                    <Switch>
-                        <Route path="/">
-                            <Home />
-                        </Route>
-                    </Switch>
-                    <Footer />
-           </div>
-        );
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+            <Footer />
+        </div>
+    );
 }
 
 export default Main;
