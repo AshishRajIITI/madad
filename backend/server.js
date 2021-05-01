cfa3e377b743666429a0f2903a48720ee8dac990
gg-dev-05 <garvitgalgat@gmail.com>
2021-05-01 16:34:44 +0530

    package.json updated

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,38 @@
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
+const cors = require("cors");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const donorRouter = require('./routes/donor');
+const donorRouter = require("./routes/donor");
 
-const seekerRouter = require('./routes/seeker');
+const seekerRouter = require("./routes/seeker");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 const uri = process.env.MONGO_DB_ATLAS_URL;
 
-mongoose.connect(uri, {useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false });
+mongoose.connect(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+});
 
 const connection = mongoose.connection;
 
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+connection.once("open", () => {
+	console.log("MongoDB database connection established successfully");
+});
 
- app.use('/donors',donorRouter);
- app.use('/seekers', seekerRouter);
+app.use("/donors", donorRouter);
+app.use("/seekers", seekerRouter);
 
-app.listen(port, ()=>{    
-
-    console.log(`Server is running at port no. : ${port}`);
-})
-
+app.listen(port, () => {
+	console.log(`Server is running at port no. : ${port}`);
+});
