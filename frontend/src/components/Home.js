06fb1037befb89e3f4b27fc5e89b1798493aea60
Akash Gupta <agbichpuriya@gmail.com>
2021-05-04 10:28:46 +0530

    fixed ui

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
         <div className="container ">
             <div className="row align-items-center">
                 <div id="homeText" className=" col-12 col-md-5 text-wrap align-middle">
-                    <div className="line2 h1">An IIT Indore Initiative to connect covid affected people to verified relief resources. Let's defeat COVID-19 together.</div>
+                    <div className="line2 h1 p-3">A Students' Gymkhana, IIT Indore initiative to connect covid affected people to verified relief resources. Let's defeat COVID-19 together.</div>
                 </div>
                 <div className="col-12 col-md-7 ">
                     <img src={network} width="100%" height="auto" alt="" />
@@ -50,42 +50,50 @@ const Home = () => {
                 </Modal>
             </div>
             <div>
-              <h1  id="help"className="text-center mt-5" >Help Them to Help You</h1>
+              <h1  id="help" className="text-center mt-5" >Help Them to Help You</h1>
               <br />
             </div>
             <div className="row mt-2 justify-content-center">
 
-                <div className=" col-12 col-md-3 ">
+                <div className=" col-12 col-md-3 my-2 ">
+                <a href="https://www.pmcares.gov.in/en/web/contribution/donate_india" rel="noreferrer" target="_blank">
                     <Card className=" donationCard">
                         <CardImg top width="100%" src={pmCaresImage} alt="Card image cap" />
                         <CardBody>
-                            <CardTitle tag="h5">PM-Cares</CardTitle>                           
+                            <CardTitle className="text-center" tag="h5">PM-Cares</CardTitle>                           
                         </CardBody>
                     </Card>
+                    </a>
                 </div>
-                <div className="col-12 col-md-3 ">
+                <div className="col-12 col-md-3 my-2">
+                <a href="https://www.bhimupi.org.in/donation-digitized-with-bhim-upi" rel="noreferrer" target="_blank">
                     <Card className=" donationCard">
                         <CardImg top width="100%" src={cmCaresImage} alt="Card image cap" />
                         <CardBody>
-                            <CardTitle tag="h5">State List</CardTitle>                           
+                            <CardTitle  className="text-center" tag="h5">State List</CardTitle>                           
                         </CardBody>
                     </Card>
+                    </a>
                 </div>
-                <div className=" col-12 col-md-3 ">
+                <div className=" col-12 col-md-3 my-2">
+                    <a href="https://www.instamojo.com/@soodcharityfoundation/lcbf43d4ae0824ea4a4118175cd8e9d28/" rel="noreferrer" target="_blank">
                     <Card className=" donationCard">
                         <CardImg top width="100%" src={soodFoundation} alt="Card image cap" />
                         <CardBody>
-                            <CardTitle tag="h5">Sood-Foundation</CardTitle>                           
+                            <CardTitle  className="text-center" tag="h5">Sood-Foundation</CardTitle>                           
                         </CardBody>
                     </Card>
+                    </a>
                 </div>
-                <div className=" col-12 col-md-3 ">
+                <div className=" col-12 col-md-3 my-2">
+                    <a href="https://www.akshayapatra.org/covid-relief-services" rel="noreferrer" target="_blank">
                     <Card className=" donationCard">
                         <CardImg top width="100%" src={akshayPatra} alt="Card image cap" />
                         <CardBody>
-                            <CardTitle tag="h5">Akshay Patra</CardTitle>                           
+                            <CardTitle  className="text-center" tag="h5">Akshay Patra</CardTitle>                           
                         </CardBody>
                     </Card>
+                    </a>
                 </div>
             </div>
             {/* <div>
