8b73007064c34e4b31f87b6d1e683594d1621182
Soham Kapileshwar <sohamkapileshwar2@gmail.com>
2021-05-06 01:15:15 +0530

    Updated seeker routes

--- backend/routes/seeker.js
+++ backend/routes/seeker.js
@@ -11,7 +11,7 @@ const twitter = require("twitter-lite");
 const client = new twitter(config);
 
 router.route("/").get((req, res) => {
-    Seeker.find()
+    Seeker.find({ isCompleted: false }).populate('user', ['name', 'email', 'mobileNumber'])
         .then((seekerList) => res.json(seekerList))
         .catch((err) => res.status(400).json("Error:" + err));
 });
@@ -29,7 +29,7 @@ router.route("/").post((req, res) => {
                 res.status(400).send(err);
             } else {
                 if (result === null) return res.status(400).send({ auth: false, message: 'User not found' });
-                
+
                 const address = req.body.address;
                 const city = req.body.city;
                 const services = req.body.services;
@@ -87,15 +87,62 @@ router.route("/").post((req, res) => {
                     .then((new_seeker) => {
                         result.seeker.push(new_seeker._id);
                         result.save();
-                        // client
-                        //     .post("statuses/update", { status: `${tweet}` })
-                        //     .then((result) => {
-                        //         // console.log('You successfully tweeted this : "' + result.text + '"');
-                        //     })
-                        //     .catch((err) => console.error);
+                        client
+                            .post("statuses/update", { status: `${tweet}` })
+                            .then((result) => {
+                                // console.log('You successfully tweeted this : "' + result.text + '"');
+                            })
+                            .catch((err) => console.error);
                         res.json(newSeeker);
                     })
                     .catch((err) => res.status(400).json("Error" + err));
+            }
+        });
+    });
+});
+
+
+router.route('/:id').put((req, res) => {
+
+    const id = req.params.id;
+
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+    jwt.verify(token, process.env.secret, function (err, decoded) {
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+        User.findById(decoded.id, function (err, user) {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                if (user === null) return res.status(400).send({ auth: false, message: 'User not found' });
+
+                const seekerList = user.seeker;
+                const seekercheck = seekerList.includes(id);
+
+                if (seekercheck) {
+                    Seeker.findById(id, function (err, seeker_object) {
+                        if (err) {
+                            res.status(400).send(err);
+                        }
+                        else {
+                            seeker_object.city = req.body.city ? req.body.city : seeker_object.city;
+                            seeker_object.services = req.body.services ? req.body.services : seeker_object.services;
+                            seeker_object.address = req.body.address ? req.body.address : seeker_object.address;
+                            seeker_object.comments = req.body.comments ? req.body.comments : seeker_object.comments;
+                            seeker_object.extra = req.body.extra ? req.body.extra : seeker_object.extra;
+                            seeker_object.isCompleted = req.body.isCompleted ? req.body.isCompleted : seeker_object.isCompleted;
+                            seeker_object.save(() => {
+                                res.status(200).send({ message: 'Updated Successfully' });
+                            });
+                        }
+                    });
+
+                }
+                else {
+                    res.status(400).send({ message: 'Update Denied' });
+                }
             }
         });
     });
